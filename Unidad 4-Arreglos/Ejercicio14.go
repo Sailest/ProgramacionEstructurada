229883bca1d7771cedda32f261dc27c878ca2501
Sailest <43819431+Sailest@users.noreply.github.com>
2019-03-14 00:35:53 -0600

    QA Cambiar el codigo

--- Unidad 4-Arreglos/Ejercicio14.go
+++ Unidad 4-Arreglos/Ejercicio14.go
@@ -19,3 +19,17 @@ func main() {
 	fmt.Println("Veces que se repite: ")
 	fmt.Println(strings.Count(inputParrafo, inputCadena))
 }
+
+/*
+	QA: Amílcar A. Ramírez Patrón
+	
+	
+	Entrada: Esta es una cadena de caracteres
+	(Falta una entrada, no deja leer la segunda cadena de caracteres)
+	Salida: 0
+	
+	-El programa funciona de forma incorrecta. No lee una cadena de texto ni un párrafo, a la vez que no evalúa cuantas veces 
+	cabe la cadena dentro del párrafo ni evalúa si el párrafo contiene siquiera texto. A su vez, sólo lee el tamaño de los valores
+	de la variable si es que no haces ningún cambio en ella como por ejemplo ingresar los datos que debes dentro de la linea 16 
+	(el scanf a la variable inputCadena).
+*/
